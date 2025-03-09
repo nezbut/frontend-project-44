1575ff883c9b85c138b4bff37241b7d11ee42f91
nezbut <anonim@anonim.com>
2025-03-10 04:41:43 +0700

    feat(src): added a new game: is prime

---
+++ src/games/prime.js
@@ -0,0 +1,18 @@
+import { getRandomNumber } from "../utils.js";
+
+function isPrime(number) {
+  if (number < 2) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+export const desc =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+export function generateRound() {
+  const number = getRandomNumber(1, 100);
+  const correctAnswer = isPrime(number) ? "yes" : "no";
+  return [String(number), correctAnswer];
+}
