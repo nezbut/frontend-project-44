deca602db72c02f23aa33dbfd93d3448b8bd5825
nezbut <anonim@anonim.com>
2025-03-10 04:25:05 +0700

    feat(src): added a new game: gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,19 @@
+import { getRandomNumber } from "../utils.js";
+
+function calculateGCD(a, b) {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+export const desc = "Find the greatest common divisor of given numbers.";
+
+export function generateRound() {
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const correctAnswer = String(calculateGCD(num1, num2));
+  return [`${num1} ${num2}`, correctAnswer];
+}
